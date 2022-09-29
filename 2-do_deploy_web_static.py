83000b12551b6a1f265b2d48a55b495013a832e3
kwamboka3 <pkwamboka@flag42.com>
2022-09-29 16:20:14 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-'''Fabric module'''
-from fabric.api import env, run, put
-from os import path
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['3.236.44.4', '3.235.147.115']
 
 
 def do_deploy(archive_path):
-    '''Distributes an archive to your web servers,
-    using the function do_deploy'''
-    filename = archive_path[9:-4]
-
-    if not path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(filename + ".tgz", filename))
-        run('rm /tmp/{}'.format(filename + '.tgz'))
-        run('mv /data/web_static/releases/{}/web_static/* '
-            ' /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(filename))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
-    return False
