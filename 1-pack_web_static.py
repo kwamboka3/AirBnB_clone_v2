e3b87c05845785ae790cb94debcdbce281e82092
kwamboka3 <pkwamboka@flag42.com>
2022-09-29 14:41:11 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""Generate .tgz file from the contents of the web_static folder"""
-
-from fabric import api
-from datetime import datetime
-import os
+'''Fabric Module'''
+from fabric.api import local
+import datetime
+from os import path
 
 
 def do_pack():
-    """Function to create tarball of webstatic files from the web_static
-    folder in Airbnb_v2.
-    Returns: path of .tgz file on success, None otherwise
-    """
-    with api.settings(warn_only=True):
-        isdir = os.path.isdir('versions')
-        if not isdir:
-            mkdir = api.local('mkdir versions')
-            if mkdir.failed:
-                return None
-        suffix = datetime.now().strftime('%Y%m%d%M%S')
-        path = 'versions/web_static_{}.tgz'.format(suffix)
-        tar = api.local('tar -cvzf {} web_static'.format(path))
-        if tar.failed:
+    '''Create a tarball file of web static'''
+    fecha = datetime.datetime.now().isoformat()
+    fecha = fecha[:-7].replace(":", "").replace(
+        ".", "").replace("T", "").replace("-", "")
+    filename = "versions/web_static_" + fecha + ".tgz"
+    if not path.exists('versions'):
+        if local("mkdir -p versions").failed:
             return None
-        size = os.stat(path).st_size
-        print('web_static packed: {} -> {}Bytes'.format(path, size))
-        return path
+    if local("tar -czvf {} web_static".format(filename)).failed:
+        return None
+    return filename
