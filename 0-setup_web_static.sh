6a9c1d1c719be8bf17fc33d82506596717ada38d
kwamboka3 <pkwamboka@flag42.com>
2022-09-29 14:39:39 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
-# Script using bash to setup webservers for deployment of simple HTML
-# content
-
-apt-get update && \
-apt-get install -y nginx && \
-mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
-echo 'Testing 123' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
-sed -i "37i $insert" /etc/nginx/sites-available/default
+# sets up your web servers for the deployment of web_static.
+sudo apt-get update -y
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Hello" > /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data
+chgrp -R ubuntu /data/
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+    
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        index index.html index.htm;
+    }    
+    
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4/;
+    }
+    
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 service nginx restart
-exit 0
