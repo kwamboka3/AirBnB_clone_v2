6f01bacb13d1cb96b71fd07ec5a3700ca3ea38fe
kwamboka3 <pkwamboka@flag42.com>
2022-09-29 14:46:39 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-"""Deploy an archive of static html to my web servers with Fabric3"""
+'''Fabric module'''
+from fabric.api import env, run, put
+from os import path
 
-from fabric import api
-from fabric.contrib import files
-import os
-
-
-api.env.hosts = ['142.44.167.235', '144.217.246.199']
-api.env.user = 'ubuntu'
-api.env.key_filename = '~/.ssh/holberton'
+env.hosts = ['35.231.45.150', '34.74.42.82']
 
 
 def do_deploy(archive_path):
-    """Function to transfer `archive_path` to web servers.
-    Args:
-        archive_path (str): path of the .tgz file to transfer
-    Returns: True on success, False otherwise.
-    """
-    if not os.path.isfile(archive_path):
+    '''Distributes an archive to your web servers,
+    using the function do_deploy'''
+    filename = archive_path[9:-4]
+
+    if not path.exists(archive_path):
         return False
-    with api.cd('/tmp'):
-        basename = os.path.basename(archive_path)
-        root, ext = os.path.splitext(basename)
-        outpath = '/data/web_static/releases/{}'.format(root)
-        try:
-            putpath = api.put(archive_path)
-            if files.exists(outpath):
-                api.run('rm -rdf {}'.format(outpath))
-            api.run('mkdir -p {}'.format(outpath))
-            api.run('tar -xzf {} -C {}'.format(putpath[0], outpath))
-            api.run('rm -f {}'.format(putpath[0]))
-            api.run('mv -u {}/web_static/* {}'.format(outpath, outpath))
-            api.run('rm -rf {}/web_static'.format(outpath))
-            api.run('rm -rf /data/web_static/current')
-            api.run('ln -sf {} /data/web_static/current'.format(outpath))
-            print('New version deployed!')
-        except:
-            return False
-        else:
-            return True
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename + ".tgz", filename))
+        run('rm /tmp/{}'.format(filename + '.tgz'))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            ' /data/web_static/releases/{}/'.format(filename, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(filename))
+        return True
+    except:
+        return False
+    return False
